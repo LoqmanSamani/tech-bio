9242b9f33ecba3f39db83709ce6f10e60ab6b51f
sam <samaniloqman91@gmail.com>
2023-12-26 14:49:27 +0100

    first commit

---
+++ uploade_data.R
@@ -0,0 +1,24 @@
+
+library(RPostgreSQL)
+library(readxl)
+
+
+host <- "82.165.61.139"
+port <- 5430
+dbname <- "Veg"
+user <- "TechBio"
+password <- "T3chB10!"
+
+
+#con =  dbConnect(dbDriver("PostgreSQL"), host = host, port = port, dbname = dbname, user = user, password = password)
+
+csv_data <- read.csv("/home/sam/Desktop/data.csv")
+
+dbWriteTable(con, name = "BodyInfo", value = csv_data, row.names = FALSE, append = TRUE)
+
+cat("CSV/Excel data inserted successfully.\n")
+
+my_table <- dbReadTable(con, "BodyInfo")
+print(my_table)
+
+

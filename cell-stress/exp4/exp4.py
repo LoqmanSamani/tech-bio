42cf8c57f6130827ada0b86bb94749eaf7e3d472
LoqmanSamani <samaniloqman91@gmail.com>
2024-07-21 19:12:53 +0200

    exp2 update

--- cell-stress/exp4/exp4.py
+++ cell-stress/exp4/exp4.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-excel_file = r'C:\Users\yda2\Desktop\exp4-ht29.xlsx'
+excel_file = r'C:\Users\yda2\Desktop\ht29.xlsx'
 df = pd.read_excel(excel_file)
 
 
@@ -9,7 +9,7 @@ csv_file = 'exp4-ht29.csv'
 df.to_csv(csv_file, index=False)
 
 
-excel_file = r'C:\Users\yda2\Desktop\exp4-hela.xlsx'
+excel_file = r'C:\Users\yda2\Desktop\hela.xlsx'
 df = pd.read_excel(excel_file)
 
 
